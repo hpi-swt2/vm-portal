8f31f903e5be7eb0e13c9b4c9a619da8b2d2957b
Christoph Matthies <chrisma@users.noreply.github.com>
2018-11-09 18:54:04 +0100

    Fix Rubocop offenses for models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "can be created using the User factory" do
-    @user = FactoryBot.create(:user)
-    expect(@user.email).to include '@'
+  it 'can be created using the User factory' do
+    user = FactoryBot.create(:user)
+    expect(user.email).to include '@'
   end
 end
