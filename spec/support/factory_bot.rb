7f0d2f7c3ea45708d7a7e24188e410613cb7b1b6
Christoph Matthies <chrisma@users.noreply.github.com>
2018-11-09 16:54:53 +0100

    Added import for FactoryBot

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,10 @@
+# Configuration for using FactoryBot in rspec
+# https://stackoverflow.com/questions/48091582/
+# https://github.com/thoughtbot/factory_bot_rails
+
+require 'factory_bot'
+
+RSpec.configure do |config|
+    # Include FactoryBot functions
+    config.include FactoryBot::Syntax::Methods
+end
