15c347a58e2ccc3db750b94f583b50c8e191cb38
Arne Boockmeyer <github@arneboockmeyer.de>
2018-11-12 13:30:26 +0100

    Set up a multistage system (#41)
    * Initialise multistaging
    
    * Move settings to the correct files
    
    * Point staging system to correct branch
    
    * Point to correct staging vm
    
    * Add comment for mina-multistage gem
    
    * Comments for staging and production mina configs
    
    * Add comments for different urls in deployment configs

---
+++ config/deploy/production.rb
@@ -0,0 +1,11 @@
+# Configuration for mina production deployment.
+# Loaded from config/deploy.rb
+
+# The hostname to SSH to (master)
+set :domain, 'vm-swt-hrmt-master.eaalab.hpi.uni-potsdam.de'
+# Path to deploy into
+set :deploy_to, '/var/www/vm-swt-hrmt-master.eaalab.hpi.uni-potsdam.de/'
+# Git repo to clone from. (needed by mina/git)
+set :repository, 'git@github.com:hpi-swt2/vm-portal.git'
+# Branch name to deploy. (needed by mina/git)
+set :branch, 'master'
