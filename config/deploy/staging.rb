ef5615f0c471706c6319c66077497e5a12b1ad79
Christoph Matthies <chrisma@users.noreply.github.com>
2018-11-12 13:12:00 +0100

    Comments for staging and production mina configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,11 @@
+# Configuration for mina dev deployment.
+# Loaded from config/deploy.rb
+
+# The hostname to SSH to
 set :domain, 'vm-swt-hrmt-dev.eaalab.hpi.uni-potsdam.de'
+# Path to deploy into
 set :deploy_to, '/var/www/vm-swt-hrmt-master.eaalab.hpi.uni-potsdam.de/'
+# Git repo to clone from. (needed by mina/git)
 set :repository, 'git@github.com:hpi-swt2/vm-portal.git'
+# Branch name to deploy. (needed by mina/git)
 set :branch, 'dev'
