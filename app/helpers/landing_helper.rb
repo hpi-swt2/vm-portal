4abde1fde4542a0ec81f7829887bfde56da29df4
Hannes Kohlsaat <hkohlsaat@gmail.com>
2018-11-12 19:15:03 +0100

    generating mostly empty landing page #10

---
+++ app/helpers/landing_helper.rb
@@ -0,0 +1,4 @@
+module LandingHelper
+	# Don't forget to write tests into spec/helpers/landing_helper_spec.rb
+	# when editing this file.
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   devise_for :users, path: 'users'
   resources :vm
+
+  root 'landing#index'
 end
