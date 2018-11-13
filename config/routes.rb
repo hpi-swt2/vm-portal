606c6396e17cc61697eeba452c4d6bb425a9d9ce
Christoph Matthies <chrisma@users.noreply.github.com>
2018-11-13 09:41:20 +0100

    Redirect '/' to vm overview

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root to: redirect('/vm')
+
   devise_for :users, path: 'users'
   resources :vm
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
