aa3552571f5fc9bc6031606f0c8592e69778026d
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-19 11:32:52 +0100

    More style fixes

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
+#
 class LandingController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- spec/views/landing/index.html.erb_spec.rb
+++ spec/views/landing/index.html.erb_spec.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'landing/index.html.erb', type: :view do
