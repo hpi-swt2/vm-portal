e814ed42737c39e98fa26941276f3c78e181888d
Marc André Freiheit <marcandre@freiheit.software>
2018-11-19 09:17:48 +0100

    Add test user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Test Users
+User.create(email: 'test@test.de', password: 'test')
