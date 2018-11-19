efebf2a4a43c93c72b9ae4b0f6d911b12ee8fd5f
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-19 11:20:43 +0100

    Fix rubocop issues

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
-describe "Index page", :type => :feature do
-
-  it "should have a navbar" do
+describe 'Index page', type: :feature do
+  it 'should have a navbar' do
     visit root_path
-    expect(page).to have_css("nav")
+    expect(page).to have_css('nav')
   end
 
-  it "should have content" do
+  it 'should have content' do
     visit root_path
-    expect(page).to have_css("#content")
+    expect(page).to have_css('#content')
   end
 
-  it "should have a footer" do
+  it 'should have a footer' do
     visit root_path
-    expect(page).to have_css("#footer")
+    expect(page).to have_css('#footer')
   end
 end
