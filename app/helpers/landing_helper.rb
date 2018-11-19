36d00dcfc1bed3e33535615129a6c4343a531fa7
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-19 11:34:23 +0100

    Finalize code style

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module LandingHelper
-# Don't forget to write tests into spec/helpers/landing_helper_spec.rb when editing this file.
+  # Don't forget to write tests into spec/helpers/landing_helper_spec.rb when editing this file.
 end
