43214a14646cabb5cdeec770120c94adaf535823
lassesteffen <lasse.steffen@t-online.de>
2018-11-19 10:11:15 +0100

    Add user profile

---
+++ app/models/user_profile.rb
@@ -0,0 +1,5 @@
+class UserProfile < ActiveRecord::Base
+
+  # Associations
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_135924) do
+ActiveRecord::Schema.define(version: 2018_11_19_090111) do
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
