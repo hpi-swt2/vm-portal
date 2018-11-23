2612101354d4d273270812528351283d06356f31
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-11-23 09:42:22 +0100

    added request vaidation

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,8 @@
 
 class Request < ApplicationRecord
   enum status: %i[pending accepted rejected]
+  validates :name, :cpu_cores, :ram_mb, :storage_mb, :operating_system, presence: true
+  validates :cpu_cores, numericality: { greater_than: 0, less_than: 65 }
+  validates :ram_mb, numericality: { greater_than: 0, less_than: 257_000 }
+  validates :storage_mb, numericality: { greater_than: 0, less_than: 1_000_000 }
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -3,5 +3,68 @@
 require 'rails_helper'
 
 RSpec.describe Request, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:request) { FactoryBot.build :request }
+
+  context 'when request is invalid' do
+    it 'is invalid with no name' do
+      request.name = ''
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with no cpu_cores specifiation' do
+      request.cpu_cores = nil
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with no ram specifiation' do
+      request.ram_mb = nil
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with no storage specifiation' do
+      request.storage_mb = nil
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with no operating_system specification' do
+      request.operating_system = ''
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with negative cpu_cores specifiation' do
+      request.cpu_cores = -1
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with to many cpu_cores ' do
+      request.cpu_cores = 65
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with negative ram specifiation' do
+      request.ram_mb = -1
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with to much ram' do
+      request.ram_mb = 257_000
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with negative storage specifiation' do
+      request.storage_mb = -1
+      expect(request).to be_invalid
+    end
+
+    it 'is invalid with to much storage' do
+      request.storage_mb = 1_000_000
+      expect(request).to be_invalid
+    end
+  end
+
+  context 'when request is valid' do
+    it 'is valid with valid attributes' do
+      expect(request).to be_valid
+    end
+  end
 end
