f0ded0116e765ccbb5a3a1ddcae879cbca316646
Adrian Steppat <adrian.steppat@student.hpi.de>
2018-11-26 14:28:43 +0100

    Fix tests so you are logged in as the user who created the notification

---
+++ spec/features/notifications_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
+
+describe 'Index page', type: :feature do
+  before do
+    user = create(:user)
+    login_as(user, scope: :user)
+    @notification = FactoryBot.create(:notification, user: user)
+  end
+  it 'has notifications with title' do
+    visit notifications_path
+
+    expect(page).to have_text(@notification.title)
+  end
+
+  it 'has notifications with messages' do
+    visit notifications_path
+
+    expect(page).to have_text(@notification.message)
+  end
+
+  it 'marks notification as read' do
+    visit mark_as_read_notification_path(@notification)
+
+    expect(@notification.reload.read).to be(true)
+  end
+end
