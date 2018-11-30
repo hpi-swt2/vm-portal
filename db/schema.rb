babcef0ecb459ba06190e6bb9d3e6d9eaf55df69
lassesteffen <lasse.steffen@t-online.de>
2018-11-30 14:09:39 +0100

    Added detail and overview page

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,6 +22,15 @@ ActiveRecord::Schema.define(version: 20_181_130_150_921) do
     t.integer 'status', default: 0
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'user_profiles', force: :cascade do |t|
+    t.integer 'user_id'
+    t.string 'first_name'
+    t.string 'last_name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_user_profiles_on_user_id'
   end
 
   create_table 'user_profiles', force: :cascade do |t|
