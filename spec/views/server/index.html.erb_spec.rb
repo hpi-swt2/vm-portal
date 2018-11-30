eec5238ad34432cf0d1c9da059cb74c3ebc8713b
JoshCoder42 <joshua.ziemann@gmail.com>
2018-11-30 14:18:15 +0100

    adapt view tests to expect links instead of buttons

--- spec/views/server/index.html.erb_spec.rb
+++ spec/views/server/index.html.erb_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe 'server/index.html.erb', type: :view do
   end
 
   it 'links to resource detail pages' do
-    expect(rendered).to have_button('Details', count: 1)
+    expect(rendered).to have_link(host[:name], count: 1)
   end
 
   it 'can filter resources' do
