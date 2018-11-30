7c63834e770c675d8def18ed37285121f97a2da2
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-30 16:16:35 +0100

    Add user roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe User, type: :model do
     user = FactoryBot.create(:user)
     expect(user.email).to include '@'
   end
+
+  it 'can be a user' do
+    user = FactoryBot.create(:user, role: :user)
+    expect(user.user?).to be_truthy
+    expect(user.wimi?).to be_falsey
+    expect(user.admin?).to be_falsey
+  end
+
+  it 'can be a wimi' do
+    user = FactoryBot.create(:user, role: :wimi)
+    expect(user.user?).to be_falsey
+    expect(user.wimi?).to be_truthy
+    expect(user.admin?).to be_falsey
+  end
+
+  it 'can be an admin' do
+    user = FactoryBot.create(:user, role: :admin)
+    expect(user.user?).to be_falsey
+    expect(user.wimi?).to be_falsey
+    expect(user.admin?).to be_truthy
+  end
 end
