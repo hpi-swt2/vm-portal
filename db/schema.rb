2760799760c32cd01b27f6c232bb66c66ce66a49
lassesteffen <lasse.steffen@t-online.de>
2018-11-30 15:16:01 +0100

    Added user profile fields to overview

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_181_130_150_921) do
-  create_table 'requests', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'cpu_cores'
-    t.integer 'ram_mb'
-    t.integer 'storage_mb'
-    t.string 'operating_system'
-    t.text 'comment'
-    t.text 'rejection_information'
-    t.integer 'status', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
-  create_table 'user_profiles', force: :cascade do |t|
-    t.integer 'user_id'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_user_profiles_on_user_id'
-  end
-
   create_table 'user_profiles', force: :cascade do |t|
     t.integer 'user_id'
     t.string 'first_name'
@@ -53,5 +31,19 @@ ActiveRecord::Schema.define(version: 20_181_130_150_921) do
     t.integer 'role'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
+
+  create_table 'requests', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'cpu_cores'
+    t.integer 'ram_mb'
+    t.integer 'storage_mb'
+    t.string 'operating_system'
+    t.string 'software'
+    t.text 'comment'
+    t.text 'rejection_information'
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 end
