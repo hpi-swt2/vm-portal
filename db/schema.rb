0332c4446b57b4542244cb598d46b38fca83910e
Max Schneider <38442947+MaxSchneider1337@users.noreply.github.com>
2018-12-01 10:13:03 +0100

    Restful Paths (#130)
    * migrated so we dont have schema conflicts
    
    * rubocop autocorrect

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,20 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_181_119_090_111) do
+  create_table 'requests', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'cpu_cores'
+    t.integer 'ram_mb'
+    t.integer 'storage_mb'
+    t.string 'operating_system'
+    t.string 'software'
+    t.text 'comment'
+    t.text 'rejection_information'
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
   create_table 'user_profiles', force: :cascade do |t|
     t.integer 'user_id'
     t.string 'first_name'
@@ -32,19 +46,5 @@ ActiveRecord::Schema.define(version: 20_181_119_090_111) do
     t.datetime 'updated_at', null: false
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-  end
-
-  create_table 'requests', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'cpu_cores'
-    t.integer 'ram_mb'
-    t.integer 'storage_mb'
-    t.string 'operating_system'
-    t.string 'software'
-    t.text 'comment'
-    t.text 'rejection_information'
-    t.integer 'status', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
   end
 end
