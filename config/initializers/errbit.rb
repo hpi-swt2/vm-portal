be28a9272df66d8ffe23311d8145abc097ab663d
Christoph Matthies <chrisma@users.noreply.github.com>
2018-12-04 15:21:50 +0100

    Fix typo and linting issues

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Airbrake.configure do |config|
   config.host = 'https://errbit-vm-portal.herokuapp.com'
   config.project_id = 1 # required, but any positive integer works
-  config.project_key = ENV['ERRBIT_PROJCT_KEY'] || '7442c4230459a58a49afb8a0bcaff0a8'
+  config.project_key = ENV['ERRBIT_PROJECT_KEY'] || '7442c4230459a58a49afb8a0bcaff0a8'
 
   # for Rails apps
   config.environment = Rails.env
