470189d0318cb3818fe7b794f1e23bff8f3d5e33
Christoph Matthies <chrisma@users.noreply.github.com>
2018-12-04 15:18:20 +0100

    Add configuration for Errbit and document it

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,9 @@
+Airbrake.configure do |config|
+  config.host = 'https://errbit-vm-portal.herokuapp.com'
+  config.project_id = 1 # required, but any positive integer works
+  config.project_key = ENV['ERRBIT_PROJCT_KEY'] || '7442c4230459a58a49afb8a0bcaff0a8'
+
+  # for Rails apps
+  config.environment = Rails.env
+  config.ignore_environments = %w(development test)
+end
