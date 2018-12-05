88b43ca51f393288f698c4cedce6f3987d39dbcd
lassesteffen <lasse.steffen@t-online.de>
2018-12-05 15:58:23 +0100

    added specs for showing all users

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,5 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe 'users/index.html.erb', type: :view do
-  pending 'add some examples to (or delete) #{__FILE__}'
+  let(:users) do
+    [
+        FactoryBot.create(:user),
+        FactoryBot.create(:user, email: "Joe@joe.de"),
+    ]
+  end
+
+  before do
+    assign(:users, users)
+    render
+  end
+
+  it 'shows all users first names' do
+    expect(rendered).to include users[0].user_profile.first_name
+    expect(rendered).to include users[1].user_profile.first_name
+  end
+
+  it 'shows all users last names' do
+    expect(rendered).to include users[0].user_profile.last_name
+    expect(rendered).to include users[1].user_profile.last_name
+  end
+
+  it 'shows a list of all users emails' do
+    expect(rendered).to include users[0].email
+    expect(rendered).to include users[1].email
+  end
 end
