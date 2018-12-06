34041a9c291b14c3a7071ab7514a669b3f03ce4a
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-12-06 22:12:19 +0100

    removed operations system from schema

--- db/schema.rb
+++ db/schema.rb
@@ -19,12 +19,6 @@ ActiveRecord::Schema.define(version: 20_181_203_191_247) do
     t.datetime 'updated_at', null: false
   end
 
-  create_table 'operation_systems', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
   create_table 'requests', force: :cascade do |t|
     t.string 'name'
     t.integer 'cpu_cores'
