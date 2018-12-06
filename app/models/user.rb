2b51cd0834775f8af115e2ac28a0c258b7e0acc7
Maximilian <koenigmax1@web.de>
2018-12-06 17:36:21 +0100

    Add the option to assign users to requests

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :users_assigned_to_requests
+  has_many :requests, through: :users_assigned_to_requests
   has_one :user_profile
   accepts_nested_attributes_for :user_profile
 end
