ecb1b5c312592bfd3e393899b6ef6b62cb562fac
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-12-07 11:04:17 +0100

    Restrict access to request functionality (#160)
    * Restrict access to request functionality
    
    * [CodeFactor] Apply fixes
    
    * Put string in locale file
    
    * Add error message

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@
 Rails.application.routes.draw do
   resources :requests, path: '/vms/requests'
 
-  root to: redirect('/users/sign_in')
+  root to: redirect('/vms')
 
   get '/servers/:id' => 'servers#show', constraints: { id: /.*/ }
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_150921) do
-
+ActiveRecord::Schema.define(version: 20_181_130_150_921) do
   create_table 'requests', force: :cascade do |t|
     t.string 'name'
     t.integer 'cpu_cores'
@@ -67,5 +68,4 @@ ActiveRecord::Schema.define(version: 2018_11_30_150921) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
