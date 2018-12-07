54eb2ad24042a97d33a1ca24dab8b5f75fcb935f
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2018-12-07 09:49:24 +0000

    fix CodeFactor

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -72,5 +74,4 @@ ActiveRecord::Schema.define(version: 20_181_203_191_247) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
