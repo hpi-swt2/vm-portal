2f7848f43392baa5ff29173f9e60997c800ad8c1
Leon Matthes <leon@matthes.biz>
2018-12-07 15:58:35 +0100

    try to make CodeFactor happy

--- db/migrate/20181207093210_create_request_templates.rb
+++ db/migrate/20181207093210_create_request_templates.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateRequestTemplates < ActiveRecord::Migration[5.2]
   def change
     create_table :request_templates do |t|
--- db/migrate/20181207094454_add_name_to_request_template.rb
+++ db/migrate/20181207094454_add_name_to_request_template.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNameToRequestTemplate < ActiveRecord::Migration[5.2]
   def change
     add_column :request_templates, :name, :string
