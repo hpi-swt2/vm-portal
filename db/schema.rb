5964a22e131c6ab53986cf8e256df7bbc6a69265
lassesteffen <lasse.steffen@t-online.de>
2018-12-07 15:14:11 +0100

    the schema is more correct

--- db/schema.rb
+++ db/schema.rb
@@ -11,60 +11,58 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2018_12_07_115117) do
-
-  create_table "operating_systems", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'operating_systems', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "requests", force: :cascade do |t|
-    t.string "name"
-    t.integer "cpu_cores"
-    t.integer "ram_mb"
-    t.integer "storage_mb"
-    t.string "operating_system"
-    t.text "comment"
-    t.text "rejection_information"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'requests', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'cpu_cores'
+    t.integer 'ram_mb'
+    t.integer 'storage_mb'
+    t.string 'operating_system'
+    t.text 'comment'
+    t.text 'rejection_information'
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "slack_auth_requests", force: :cascade do |t|
-    t.string "state"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_slack_auth_requests_on_user_id"
+  create_table 'slack_auth_requests', force: :cascade do |t|
+    t.string 'state'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_slack_auth_requests_on_user_id'
   end
 
-  create_table "slack_hooks", force: :cascade do |t|
-    t.string "url"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_slack_hooks_on_user_id"
+  create_table 'slack_hooks', force: :cascade do |t|
+    t.string 'url'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_slack_hooks_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.integer "role"
-    t.string "first_name"
-    t.string "last_name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.integer 'role'
+    t.string 'first_name'
+    t.string 'last_name'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
 end
