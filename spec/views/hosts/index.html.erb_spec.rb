8add80123655cecb17ac7892b664ef68aec5e4cb
Daniel Lindner <27929897+dey4ss@users.noreply.github.com>
2018-12-13 17:08:05 +0100

    show VM state on host details page (#170) (#191)
    * show resource allocation as chart on vm details page #169
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * show online status and power optoins prettier on vm detail page #169
    
    * apply lint proposals #169
    
    * include Hardware specs and User table on VM detail page #169
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * apply design update for host detail page #170, add 404 page
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * avoid line break on donut charts #170
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.uni-potsdam.de>
    
    * change colors of donut charts depending on value #169 #170
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * add tests for 404 page redirect for show vms/hosts
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * add vm status on host details page #170
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * apply lint proposals #170
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * add missing space before % sign in charts #170
    
    Co-authored-by: tomschw <tom.schwarzburg@student.hpi.de>
    
    * remove trailing whitespace

--- spec/views/hosts/index.html.erb_spec.rb
+++ spec/views/hosts/index.html.erb_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'hosts/index.html.erb', type: :view do
        vendor: 'nice vendor',
        bootTime: 'some long while',
        connectionState: 'connected',
-       vm_names: ['a name', 'another name'] }
+       vms: { 'a name' => true, 'another name' => false } }
   end
   let(:param) do
     %w[up_hosts down_hosts]
@@ -42,7 +42,7 @@ RSpec.describe 'hosts/index.html.erb', type: :view do
   end
 
   it 'renders vm names' do
-    host[:vm_names].each do |name|
+    host[:vms].keys.each do |name|
       expect(rendered).to include name
     end
   end
