164a675708d9be5d62ef9893c4f857f907908b84
Chris Schulz <christopher.schulz@student.hpi.de>
2018-12-13 23:18:12 +0100

    try to get rid of ; at in the class header for the 3 time

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SessionsController < Devise::SessionsController;
+class SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
