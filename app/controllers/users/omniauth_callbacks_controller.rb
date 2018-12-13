2398ea145750aeb67289c0f194bdf3125d753d12
Chris Schulz <christopher.schulz@student.hpi.de>
2018-12-13 23:09:36 +0100

    fix bunch of codebeat issues

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,16 +3,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      set_flash_message!(:success, :success, :kind => "HPI OpenID Connect")
+      set_flash_message!(:success, :success, :kind => 'HPI OpenID Connect')
       sign_in_and_redirect @user, event: :authentication
     else
-      set_flash_message!(:danger, :failure, :kind => "HPI OpenID Connect", :reason => "Login failed")
+      set_flash_message!(:danger, :failure, :kind => 'HPI OpenID Connect', :reason => 'Login failed')
       redirect_to root_path
     end
   end
 
   def failure
-    set_flash_message!(:danger, :failure, :kind => "HPI OpenID Connect", :reason => "Login failed")
+    set_flash_message!(:danger, :failure, :kind => 'HPI OpenID Connect', :reason => 'Login failed')
     redirect_to root_path
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,61 +12,61 @@
 
 ActiveRecord::Schema.define(version: 2018_12_09_184959) do
 
-  create_table "notifications", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "title"
-    t.string "message"
-    t.boolean "read"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_notifications_on_user_id"
+  create_table 'notifications', force: :cascade do |t|
+    t.integer 'user_id'
+    t.string 'title'
+    t.string 'message'
+    t.boolean 'read'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_notifications_on_user_id'
   end
 
-  create_table "operating_systems", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'operating_systems', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "requests", force: :cascade do |t|
-    t.string "name"
-    t.integer "cpu_cores"
-    t.integer "ram_mb"
-    t.integer "storage_mb"
-    t.string "operating_system"
-    t.text "comment"
-    t.text "rejection_information"
-    t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'requests', force: :cascade do |t|
+    t.string 'name'
+    t.integer 'cpu_cores'
+    t.integer 'ram_mb'
+    t.integer 'storage_mb'
+    t.string 'operating_system'
+    t.text 'comment'
+    t.text 'rejection_information'
+    t.integer 'status', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "slack_auth_requests", force: :cascade do |t|
-    t.string "state"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_slack_auth_requests_on_user_id"
+  create_table 'slack_auth_requests', force: :cascade do |t|
+    t.string 'state'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_slack_auth_requests_on_user_id'
   end
 
-  create_table "slack_hooks", force: :cascade do |t|
-    t.string "url"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_slack_hooks_on_user_id"
+  create_table 'slack_hooks', force: :cascade do |t|
+    t.string 'url'
+    t.integer 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_slack_hooks_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
