da0afa0de81de61537d0c6f0a0dc224834dcf1b4
Philipp Schmidt <philipp.schmidt@student.hpi.de>
2018-12-14 10:01:25 +0100

    test that all necessary links are in the progressbar

--- spec/views/application/_navbar.html.erb_spec.rb
+++ spec/views/application/_navbar.html.erb_spec.rb
@@ -10,19 +10,27 @@ RSpec.describe 'application/_navbar.html.erb', type: :view do
     render
   end
 
-  it 'links to host list' do
-    expect(rendered).to have_link('Hosts', href: hosts_path)
+  it 'links to dashboard' do
+    expect(rendered).to have_link('Dashboard', href: dashboard_path)
   end
 
   it 'links to vm list' do
     expect(rendered).to have_link('VMs', href: vms_path)
   end
 
-  it 'links to users list' do
+  it 'links to host list' do
+    expect(rendered).to have_link('Hosts', href: hosts_path)
+  end
+
+  it 'links to user list' do
     expect(rendered).to have_link('Users', href: users_path)
   end
 
-  it 'links to the current user' do
+  it 'links to the current user profile' do
     expect(rendered).to have_link(current_user.name, href: user_path(current_user))
+  end
+
+  it 'links to the Notification Page' do
+    expect(rendered).to have_link(href: notifications_path)
   end
 end
