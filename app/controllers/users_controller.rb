767fd356ec9a3fdf72f95ede616436932217f902
Jan Westphal <jan.westphal306@gmail.com>
2018-12-14 11:25:56 +0100

    Add edit user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
 
