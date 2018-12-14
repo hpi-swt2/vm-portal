a30279ddeceea9237d233dcf8ee0cd3cf768b1ae
skydivin4ng3l <christopher.schulz@student.hpi.de>
2018-12-14 12:24:27 +0100

    fix codefactor issue: Use nested module/class definitions instead of compact style.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def hpi
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+module Users
+  class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def hpi
+      @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
-      set_flash_message!(:success, :success, kind: 'HPI OpenID Connect')
-      sign_in_and_redirect @user, event: :authentication
-    else
+      if @user.persisted?
+        set_flash_message!(:success, :success, kind: 'HPI OpenID Connect')
+        sign_in_and_redirect @user, event: :authentication
+      else
+        set_flash_message!(:danger, :failure, kind: 'HPI OpenID Connect', reason: 'Login failed')
+        redirect_to root_path
+      end
+    end
+
+    def failure
       set_flash_message!(:danger, :failure, kind: 'HPI OpenID Connect', reason: 'Login failed')
       redirect_to root_path
     end
-  end
-
-  def failure
-    set_flash_message!(:danger, :failure, kind: 'HPI OpenID Connect', reason: 'Login failed')
-    redirect_to root_path
   end
 end
