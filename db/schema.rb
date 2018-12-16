7ba40765feb99b38765a9cd663da5dc86850b8fd
Maximilian <koenigmax1@web.de>
2018-12-16 13:40:50 +0100

    Merge branch '99/scaffold/add-user-to-vm-request' of https://github.com/hpi-swt2/vm-portal into 99/scaffold/add-user-to-vm-request
    # Conflicts:
    #	app/views/requests/_form.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -83,8 +83,6 @@ ActiveRecord::Schema.define(version: 2018_12_09_184959) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["request_id"], name: "index_users_assigned_to_requests_on_request_id"
-    t.index ["user_id"], name: "index_users_assigned_to_requests_on_user_id"
   end
 
 end
