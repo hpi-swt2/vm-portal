38ebb1e74210bd03d636a8195e909ed3eaf1b060
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-12-16 12:25:25 +0100

    autocorrect

--- db/migrate/20181216111931_change_request_reachability_name_column.rb
+++ db/migrate/20181216111931_change_request_reachability_name_column.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class ChangeRequestReachabilityNameColumn < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :requests, :reachability_name, :application_name
+    rename_column :requests, :reachability_name, :application_name
   end
 end
