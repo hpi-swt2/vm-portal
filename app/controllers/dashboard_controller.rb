aa39df002809e48962bb49c88da49a7c4379d59f
leonhard.hennicke <leonhard.hennicke@student.hpi.de>
2018-12-18 23:14:09 +0100

    Make codefactor happy.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,12 @@
 require 'vmapi.rb'
 class DashboardController < ApplicationController
   attr_reader :vms, :hosts, :notifications
-  @@number_of_notifications = 3
+  @number_of_notifications = 3
 
   def index
     redirect_to '/users/sign_in' if current_user.nil?
     @vms = VmApi.instance.all_vms
     @hosts = VmApi.instance.all_hosts
-    @notifications = Notification.where(user: current_user).slice(0, @@number_of_notifications)
+    @notifications = Notification.where(user: current_user).slice(0, @number_of_notifications)
   end
 end
