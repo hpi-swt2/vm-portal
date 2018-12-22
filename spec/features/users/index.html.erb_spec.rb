ca7dfe55309f6fb942a229bf803eb81f47d46a74
lassesteffen <lasse.steffen@t-online.de>
2018-12-22 15:14:18 +0100

    add test for checking that the correct user role is displayed

--- spec/features/users/index.html.erb_spec.rb
+++ spec/features/users/index.html.erb_spec.rb
@@ -4,31 +4,33 @@ require 'rails_helper'
 
 RSpec.describe 'users/index.html.erb', type: :feature do
   let(:user) { FactoryBot.create :user }
-  let(:admin) {FactoryBot.create :admin}
-  let(:employee) {FactoryBot.create :employee}
+  let(:admin) { FactoryBot.create :admin }
+  let(:employee) { FactoryBot.create :employee }
 
-  context 'when the user is an admin' do
+  context 'when the current_user is an admin' do
     before do
+      user
+      employee
       sign_in admin
       visit users_path
     end
 
-    it 'shows that the users role is users' do
-      expect(page.find_button(id: "btn-user-#{users.id}")).to have_css('active')
-      expect(page.find_button(id: "btn-employee-#{users.id}")).not_to have_css('active')
-      expect(page.find_button(id: "btn-admin-#{users.id}")).not_to have_css('active')
+    it 'shows that an users role is users' do
+      expect(page.find(id: "btn-user-#{user.id}")[:class]).to include 'active'
+      expect(page.find(id: "btn-employee-#{user.id}")[:class]).not_to include 'active'
+      expect(page.find(id: "btn-admin-#{user.id}")[:class]).not_to include 'active'
     end
 
-    # it 'shows that the employee role is employee' do
-    #   expect(page.find_button(id: "btn-user-#{employee.id}")).not_to have_css('active')
-    #   expect(page.find_button(id: "btn-employee-#{employee.id}")).to have_css('active')
-    #   expect(page.find_button(id: "btn-admin-#{employee.id}")).not_to have_css('active')
-    # end
-    #
-    # it 'shows that the admins role is admin' do
-    #   expect(page.find_button(id: "btn-user-#{admin.id}")).not_to have_css('active')
-    #   expect(page.find_button(id: "btn-employee-#{admin.id}")).not_to have_css('active')
-    #   expect(page.find_button(id: "btn-admin-#{admin.id}")).to have_css('active')
-    # end
+    it 'shows that an employees role is employee' do
+      expect(page.find(id: "btn-user-#{employee.id}")[:class]).not_to include 'active'
+      expect(page.find(id: "btn-employee-#{employee.id}")[:class]).to include 'active'
+      expect(page.find(id: "btn-admin-#{employee.id}")[:class]).not_to include 'active'
+    end
+
+    it 'shows that an admins role is admin' do
+      expect(page.find(id: "btn-user-#{admin.id}")[:class]).not_to include 'active'
+      expect(page.find(id: "btn-employee-#{admin.id}")[:class]).not_to include 'active'
+      expect(page.find(id: "btn-admin-#{admin.id}")[:class]).to include 'active'
+    end
   end
 end
