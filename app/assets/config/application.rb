7e4afacce9b61d234d6fc256099ffde652ea0327
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-12-22 19:45:41 +0100

    autocorrect

--- app/assets/config/application.rb
+++ app/assets/config/application.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Application < Rails::Application
-    config.action_dispatch.default_headers.merge!(
-      'Cache-Control' => 'no-store, no-cache'
-    )
+  config.action_dispatch.default_headers.merge!(
+    'Cache-Control' => 'no-store, no-cache'
+  )
 end
