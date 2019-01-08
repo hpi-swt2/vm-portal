4852fba9ee1f2631b270f854030fc2cb1a125a40
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2019-01-08 18:13:04 +0000

    delete helper method

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 module RequestsHelper
-	def object_as_js_var(var_name, object, content_name = nil)
-        output_tag = javascript_tag("var #{var_name} = #{object.to_json};".html_safe, type: 'text/javascript')
-        if content_name
-            content_for content_name, output_tag
-        else
-            output_tag
-        end
-    end
 end
