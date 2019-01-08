3952e87e362f1caf2503a4388ad4eba08b554b54
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2019-01-08 18:16:09 +0000

    delete unnecessary line in requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -51,7 +51,6 @@ class RequestsController < ApplicationController
   # POST /requests.json
   def create
     @request = Request.new(request_params)
-    @request_templates = RequestTemplate.all
     save_sudo_rights(@request)
 
     respond_to do |format|
