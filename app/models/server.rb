e1d4f667abe176cadb943842462723b637347140
JoshCoder42 <joshua.ziemann@gmail.com>
2019-01-11 12:19:33 +0100

    Fix model problems and improve javascript part

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,7 +2,6 @@ class Server < ApplicationRecord
   validates :name, :cpu_cores, :ram_mb, :storage_mb, :mac_address, :fqdn, presence: true
   validates :ipv4_address, presence: { unless: :ipv6_address? }
   validates :ipv6_address, presence: { unless: :ipv4_address? }
-  #validates :installed_software, presence: false
   serialize :installed_software, Array
 
   validates :cpu_cores, numericality: { greater_than: 0 }
@@ -10,11 +9,11 @@ class Server < ApplicationRecord
   validates :storage_mb, numericality: { greater_than: 0 }
   
   validates :ipv4_address, format: { 
-  	with: /((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/,
+  	with: /(^$)|((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/,
 	message: "Not a valid IPv4 address" 
 	}
   validates :ipv6_address, format: { 
-  	with: /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/,
+  	with: /(^$)|(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/,
 	message: "Not a valid IPv6 address" 
 	}
   validates :mac_address, format: { 
--- db/schema.rb
+++ db/schema.rb
@@ -28,16 +28,6 @@ ActiveRecord::Schema.define(version: 2019_01_11_102059) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "request_templates", force: :cascade do |t|
-    t.integer "cpu_cores"
-    t.integer "ram_mb"
-    t.integer "storage_mb"
-    t.string "operating_system"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-  end
-
   create_table "requests", force: :cascade do |t|
     t.string "name"
     t.integer "cpu_cores"
@@ -49,8 +39,6 @@ ActiveRecord::Schema.define(version: 2019_01_11_102059) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "port"
-    t.string "application_name"
   end
 
   create_table "servers", force: :cascade do |t|
@@ -91,17 +79,16 @@ ActiveRecord::Schema.define(version: 2019_01_11_102059) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "first_name"
-    t.string "last_name"
+    t.integer "role"
     t.string "provider"
     t.string "uid"
-    t.string "ssh_key"
+    t.string "first_name"
+    t.string "last_name"
     t.integer "user_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
