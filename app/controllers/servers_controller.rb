fe12215c2500556e07eec35a69dae2726809ef07
JoshCoder42 <joshua.ziemann@gmail.com>
2019-01-11 13:25:28 +0100

    Implments the function that only admins can create, update and destroy Servers

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,6 +1,6 @@
 class ServersController < ApplicationController
   before_action :set_server, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_admin, only: [:new, :create, :edit, :update, :destroy]
   # GET /servers
   # GET /servers.json
   def index
