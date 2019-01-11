a48958a10343d04fb40ad53a5aa2d18fc81867aa
Jan Westphal <jan.westphal306@gmail.com>
2019-01-12 00:47:15 +0100

    Add tests for user profile page

--- spec/features/users/show.html.erb_spec.rb
+++ spec/features/users/show.html.erb_spec.rb
@@ -3,18 +3,51 @@
 require 'rails_helper'
 
 describe 'users/show.html.erb', type: :feature do
-  let(:invalid_ssh_key) { 'super good key' }
+  context 'when the user is not an admin' do
+    let(:user) { FactoryBot.create :user }
+    let(:another_user) { FactoryBot.create :user }
 
-  let(:user) { FactoryBot.create :user }
+    before do
+      sign_in user
+    end
 
-  before do
-    sign_in user
-    visit user_path(user)
+    context 'when visits his profile' do
+      before do
+        visit user_path(user)
+      end
+
+      it 'does not redirect' do
+        expect(page).to have_current_path(user_path(user))
+      end
+    end
+
+    context 'when visits another users profile' do
+      before do
+        visit user_path(another_user)
+      end
+
+      it 'redirects to the dashboard' do
+        expect(page).to have_current_path dashboard_path
+      end
+    end
   end
 
-  context 'when the user has a ssh key' do
-    it 'shows the user ssh-key' do
-      expect(page).to have_content(user.ssh_key)
+  context 'when the user is an admin' do
+    let(:admin) { FactoryBot.create :admin }
+    let(:another_user) { FactoryBot.create :user }
+
+    before do
+      sign_in admin
+    end
+
+    context 'when the user visits another users profile' do
+      before do
+        visit user_path(another_user)
+      end
+
+      it 'does not redirect' do
+        expect(page).to have_current_path(user_path(another_user))
+      end
     end
   end
 end
