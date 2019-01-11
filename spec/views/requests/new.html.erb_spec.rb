0daa42bbf6bfb6f45d98882aa63881ca85d5e002
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2019-01-11 11:01:37 +0000

    sixth try to fix CodeFactor

--- spec/views/requests/new.html.erb_spec.rb
+++ spec/views/requests/new.html.erb_spec.rb
@@ -16,11 +16,11 @@ RSpec.describe 'requests/new', type: :view do
                        status: 'pending'
                      ))
     assign(:request_templates, [RequestTemplate.new(
-                                  name: 'MyTemplate',
-                                  cpu_cores: 2,
-                                  ram_mb: 1000,
-                                  storage_mb: 2000,
-                                  operating_system: 'CentOS 7'
+                                 name: 'MyTemplate',
+                                 cpu_cores: 2,
+                                 ram_mb: 1000,
+                                 storage_mb: 2000,
+                                 operating_system: 'CentOS 7'
                                 )])
     render
   end
