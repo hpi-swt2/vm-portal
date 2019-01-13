4a829fe8b06a94761566159cbac60f0464f11dbf
Hannes Kohlsaat <hkohlsaat@gmail.com>
2019-01-13 09:54:28 +0100

    Confirmation dialogs for potentially dangerous actions (#209)
    * failing test for vm shutdown confirmation #178
    
    * more failing tests #178
    
    * adding warning messages #178
    
    * more tests, fixing CodeFactor issue #178
    
    * removing warning on notification deletion #178
    
    * removing CodeFactor issues #178
    
    * fixing CodeFactor issues #178
    
    * adding warnings for Delete and various shutdown options #178
    
    * fixing CodeFactor issues #178

--- spec/views/vms/index.html.erb_spec.rb
+++ spec/views/vms/index.html.erb_spec.rb
@@ -71,6 +71,10 @@ RSpec.describe 'vms/index.html.erb', type: :view do
     expect(rendered).to have_button('Start')
     expect(rendered).to have_button('Shutdown')
   end
+  
+  it 'demands confirmation on shutdown' do
+    expect(rendered).to have_selector('input[value="Shutdown"][data-confirm="Are you sure?"]')
+  end
 
   it 'shows no power buttons when vmwaretools are not installed' do
     assign(:vms, mock_vms_without_tools)
