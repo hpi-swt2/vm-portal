d1a05396b6043f0465bc3644889a931bb0694f8a
Jan Westphal <jan.westphal306@gmail.com>
2019-01-14 13:35:52 +0100

    Restrict access to project new and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ProjectsController < ApplicationController
-  before_action :authenticate_employee
+  before_action :authenticate_employee, only: %i[new create]
 
   def index
     @projects = Project.all
