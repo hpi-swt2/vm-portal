eb5827c5244c18012c4743e63e533a2cb596d1c5
Leon Matthes <leon@matthes.biz>
2019-01-16 23:33:27 +0100

    Incorporate changes as suggested by @mlichtblau

--- spec/api/v_sphere_api_spec.rb
+++ spec/api/v_sphere_api_spec.rb
@@ -29,11 +29,9 @@ describe VSphere do
   end
 
   let(:mock_archived_vms) do
-    vms = []
-    (1..5).each do |each|
-      vms << vim_vm_mock('Archived VM' + each.to_s, power_state: 'poweredOff')
+    (1..5).map do |each|
+      vim_vm_mock('Archived VM' + each.to_s, power_state: 'poweredOff')
     end
-    vms
   end
 
   let(:mock_archived_vms_folder) do
@@ -41,11 +39,9 @@ describe VSphere do
   end
 
   let(:mock_pending_archivings_vms) do
-    vms = []
-    (1..5).each do |each|
-      vms << vim_vm_mock('Pending Archiving VM' + each.to_s)
+    (1..5).map do |each|
+      vim_vm_mock('Pending Archiving VM' + each.to_s)
     end
-    vms
   end
 
   let(:mock_pending_archivings_folder) do
@@ -53,11 +49,9 @@ describe VSphere do
   end
 
   let(:mock_root_folder_vms) do
-    vms = []
-    (1..5).each do |each|
-      vms << vim_vm_mock('Root folder VM' + each.to_s, power_state: 'poweredOff')
+    (1..5).map do |each|
+      vim_vm_mock('Root folder VM' + each.to_s, power_state: 'poweredOff')
     end
-    vms
   end
 
   let(:mock_folder) do
