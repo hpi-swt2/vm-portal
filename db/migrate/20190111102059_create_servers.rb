66c7014290a4a11579254c94283dcbdfb44f5f06
Joshua Ziemann <joshua.ziemann@gmail.com>
2019-01-17 18:22:12 +0100

    Add new Server Page (#196)
    This MR adds the new server page and is related to issue #173 .

---
+++ db/migrate/20190111102059_create_servers.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreateServers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :servers do |t|
+      t.string :name
+      t.integer :cpu_cores
+      t.integer :ram_mb
+      t.integer :storage_mb
+      t.string :mac_address
+      t.string :fqdn
+      t.string :ipv4_address
+      t.string :ipv6_address
+      t.string :installed_software, default: [].to_yaml
+
+      t.timestamps
+    end
+  end
+end
