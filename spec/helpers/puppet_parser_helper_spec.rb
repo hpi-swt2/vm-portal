3a72ee5eb38f8263c7c7aed019f6a2f77cd2e8ab
Niklas_Koehnecke <niklas.koehnecke@gmail.com>
2019-01-18 17:50:54 +0100

    Adapt puppet_helper tests

--- spec/helpers/puppet_parser_helper_spec.rb
+++ spec/helpers/puppet_parser_helper_spec.rb
@@ -3,52 +3,47 @@
 require 'rails_helper'
 
 RSpec.describe PuppetParserHelper, type: :helper do
-  describe 'Puppet Parser' do
-    it "reads a puppet file and parses it's arguments into an array" do
-      file = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example.pp')
-      puppet_file = PuppetParserHelper.readNodeFile(file)
-      admins = puppet_file.admins
-      users = puppet_file.users
+  describe 'read_node_file' do
+    let (:path) { File.join('spec', 'helpers') }
 
-      expect(puppet_file).to be_a_kind_of Dictionary
+    it 'reads a puppet file and parses it\'s arguments into an array' do
+      user = FactoryBot.create :puppet_user
+      admin1 = FactoryBot.create :puppet_admin
+      admin2 = FactoryBot.create :puppet_admin2
+      puppet_file = PuppetParserHelper.read_node_file('example', path)
+      admins = puppet_file['admins']
+      users = puppet_file['users']
+      expect(puppet_file).to be_a_kind_of Hash
 
       expect(admins).to be_a_kind_of Array
       expect(admins.count).to eq(2)
-      expect(admins[0]).to eq("Vorname.Nachname")
-      expect(admins[1]).to eq("weitererVorname.Nachname")
+      expect(admins.first).to include(admin1)
+      expect(admins.first).to include(admin2)
 
       expect(users).to be_a_kind_of Array
-      expect(users[1]).to eq("andererVorname.Nachname")
+      expect(users.first).to include(user)
     end
 
-    it "raises an error if trying to read a puppet file which does not contain admins" do
-      file = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example2.pp')
-      expect(PuppetParser.readNodeFile(file)).to raise_error
+    it 'raises an error if trying to read a puppet file which does not contain admins' do
+      expect { PuppetParserHelper.read_node_file('example2', path) }.to raise_error 'unsupported Format'
     end
 
-    it "raises an error if trying to read a puppet file with syntax errors" do
-      # syntax errors include: - file name not matching node name, example3
-      #                        - missing class declaration on first line, example7
-      #                        - missing closing bracket for class declaration, example8
-      file = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example3.pp')
-      expect(PuppetParser.readNodeFile(file)).to raise_error
-      file2 = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example7.pp')
-      expect(PuppetParser.readNodeFile(file2)).to raise_error
-      file3 = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example8.pp')
-      expect(PuppetParser.readNodeFile(file3)).to raise_error
+    # syntax errors include: - file name not matching node name, example3
+    #                        - missing class declaration on first line, example7
+    #                        - missing closing bracket for class declaration, example8
+    it 'raises an error if trying to read a puppet file with syntax errors' do
+      expect { PuppetParserHelper.read_node_file('example3', path) }.to raise_error 'unsupported Format'
+      expect { PuppetParserHelper.read_node_file('example7', path) }.to raise_error 'unsupported Format'
+      expect { PuppetParserHelper.read_node_file('example8', path) }.to raise_error 'unsupported Format'
     end
 
-    it "raises an error if trying to read a puppet file in which admins are not added as sudoroot or virtual user" do
-      file = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example4.pp')
-      expect(PuppetParser.readNodeFile(file)).to raise_error
-
-      file2 = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example5.pp')
-      expect(PuppetParser.readNodeFile(file2)).to raise_error
+    it 'raises an error if trying to read a puppet file in which admins are not added as sudoroot or virtual user' do
+      expect { PuppetParserHelper.read_node_file('example4', path) }.to raise_error 'unsupported Format'
+      expect { PuppetParserHelper.read_node_file('example5', path) }.to raise_error 'unsupported Format'
     end
 
-    it "raises an error if trying to read a puppet file in which users are not added as virtual user" do
-      file = File.join(Rails.root, 'spec', 'helpers', 'files', 'node-example6.pp')
-      expect(PuppetParser.readNodeFile(file)).to raise_error
+    it 'raises an error if trying to read a puppet file in which users are not added as virtual user' do
+      expect { PuppetParserHelper.read_node_file('example6', path) }.to raise_error 'unsupported Format'
     end
   end
 end
