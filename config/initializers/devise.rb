dc289cf039c48939ff4b979bdeea06c9bc4b549c
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2019-01-22 09:56:04 +0100

    fix small https error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -298,7 +298,7 @@ Devise.setup do |config|
                     host: 'oidc.hpi.de',
                     identifier: Rails.application.credentials.hpi_openid[:client_id],
                     secret: Rails.application.credentials.hpi_openid[:client_secret],
-                    redirect_uri: 'http://hart-dev.epic-hpi.de/',
+                    redirect_uri: 'https://hart-dev.epic-hpi.de/',
                     authorization_endpoint: '/auth'
                   },
                   client_auth_method: :other,
