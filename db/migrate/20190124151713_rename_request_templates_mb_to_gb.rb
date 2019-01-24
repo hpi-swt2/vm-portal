d159573ad103d15e80faee6bb02757baef275bd7
Maximilian <koenigmax1@web.de>
2019-01-24 17:03:29 +0100

    Add migration to rename mb to gb in request templates

---
+++ db/migrate/20190124151713_rename_request_templates_mb_to_gb.rb
@@ -0,0 +1,6 @@
+class RenameRequestTemplatesMbToGb < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :request_templates, :ram_mb, :ram_gb
+    rename_column :request_templates, :storage_mb, :storage_gb
+  end
+end
