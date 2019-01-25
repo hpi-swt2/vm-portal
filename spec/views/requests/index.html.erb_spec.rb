c9e7431ba6b4a8cd9754084271008110f01d63c1
MaximilianKoenig <koenigmax1@web.de>
2019-01-25 11:02:10 +0100

    Adjust input types for requests and request_templates (#282)
    * Adjust input types for requests and request_templates
    
    * Fix failing tests
    
    * Fix failing tests
    
    * Rename storage and ram from mb to gb in request templates
    
    * Add migration to rename mb to gb in request templates
    
    * Fix user displaying at requests show page
    
    * Add magic comment to migration
    
    * Apply requested changes
    
    * Change unless to if
    
    * Apply CodeFactory Style

--- spec/views/requests/index.html.erb_spec.rb
+++ spec/views/requests/index.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'requests/index', type: :view do
     [
       Request.create!(
         name: 'MyVM',
-        cpu_cores: 2,
+        cpu_cores: 3,
         ram_mb: 1000,
         storage_mb: 2000,
         operating_system: 'MyOS',
@@ -20,7 +20,7 @@ RSpec.describe 'requests/index', type: :view do
       ),
       Request.create!(
         name: 'MyVM1',
-        cpu_cores: 2,
+        cpu_cores: 3,
         ram_mb: 1000,
         storage_mb: 2000,
         operating_system: 'MyOS',
@@ -42,9 +42,9 @@ RSpec.describe 'requests/index', type: :view do
   it 'renders a list of requests' do
     assert_select 'tr>td', text: 'MyVM'.to_s, count: 1
     assert_select 'tr>td', text: 'MyVM1'.to_s, count: 1
+    assert_select 'tr>td', text: 3.to_s, count: 2
+    assert_select 'tr>td', text: 1.to_s, count: 2
     assert_select 'tr>td', text: 2.to_s, count: 2
-    assert_select 'tr>td', text: 1000.to_s, count: 2
-    assert_select 'tr>td', text: 2000.to_s, count: 2
     assert_select 'tr>td', text: 'MyOS'.to_s, count: 2
     assert_select 'tr>td', text: 4000.to_s, count: 2
     assert_select 'tr>td', text: 'MyName'.to_s, count: 2
