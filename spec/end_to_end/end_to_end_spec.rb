9460074f4963c043257043aab54305ce9611c9d4
arne-z <arne.zerndt@student.hpi.de>
2019-01-28 22:02:48 +0100

    Split last test

--- spec/end_to_end/end_to_end_spec.rb
+++ spec/end_to_end/end_to_end_spec.rb
@@ -42,7 +42,6 @@ RSpec.describe "End to End testing", type: :feature do
             sign_in @admin
             visit '/vms'
             click_on 'New Request'
-            expect(current_path).to eq('/vms/requests/new')
             fill_in('VM Name', :with => @requestname)
             fill_in('cpu', :with => 4)
             fill_in('ram', :with => 8)
@@ -52,8 +51,6 @@ RSpec.describe "End to End testing", type: :feature do
             select('none', :from => 'operating_system')
             fill_in('Description', :with => 'test')
             click_on 'Create Request'
-            expect(page).to have_text('Request was successfully created.')
-            expect(current_path).to eq('/vms/requests')
             click_on @requestname
             click_on 'acceptButton'
             expect(page).to have_text('Request was successfully updated and the vm was successfully created.')
@@ -65,6 +62,29 @@ RSpec.describe "End to End testing", type: :feature do
             expect(current_path).to eq('/vms/requests')
             click_on @requestname
             expect(page).to have_text('accepted')
+            visit "/vms/#{@requestname}"
+            expect(page).to have_text('offline')
+        end
+        
+        it 'is possible to turn on a VM' do
+            sign_in @admin
+            visit '/vms'
+            click_on 'New Request'
+            fill_in('VM Name', :with => @requestname)
+            fill_in('cpu', :with => 4)
+            fill_in('ram', :with => 8)
+            fill_in('storage', :with => 126)
+            select(@admin.email, :from => 'request_sudo_user_ids')
+            select(@user.email, :from => 'request_user_ids')
+            select('none', :from => 'operating_system')
+            fill_in('Description', :with => 'test')
+            click_on 'Create Request'
+            click_on @requestname
+            click_on 'acceptButton'
+            fill_in('virtual_machine_config_ip', :with => '123.0.0.23')
+            fill_in('virtual_machine_config_dns', :with => 'www.example.com')
+            click_on 'Update Configuration'
+            click_on @requestname
             visit "/vms/#{@requestname}"
             expect(page).to have_text('offline')
             select('Power On', :from => 'navbarDropdown')
