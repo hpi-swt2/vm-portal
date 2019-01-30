aad40560b3a271e081d51cb0a678e3a805ac7917
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2019-01-30 17:32:57 +0100

    add default false, not tested

--- db/migrate/20190129184200_add_email_notifications_to_user.rb
+++ db/migrate/20190129184200_add_email_notifications_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddEmailNotificationsToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :email_notifications, :boolean, default: true
+    add_column :users, :email_notifications, :boolean, default: false
   end
 end
