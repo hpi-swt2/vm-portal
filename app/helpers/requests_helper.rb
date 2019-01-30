27ccc22128659e2a8b66dbac8894e5e5111c7782
Maximilian <koenigmax1@web.de>
2019-01-30 18:33:25 +0100

    Make codefactor happy

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module RequestsHelper
-
   # replaces all whitespaces and special characters by a hyphen and makes the string to lower case
   def replace_whitespaces(name)
     name.parameterize
