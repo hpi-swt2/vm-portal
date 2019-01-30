e1e2c261e440c4443ab696d352f80c63860b2a4e
Jan Westphal <jan.westphal306@gmail.com>
2019-01-30 23:49:00 +0100

    Add requested changes and fix tests

--- spec/controllers/hosts_controller_spec.rb
+++ spec/controllers/hosts_controller_spec.rb
@@ -10,33 +10,26 @@ RSpec.describe HostsController, type: :controller do
   describe 'GET #index' do
     before do
       allow(VSphere::Host).to receive(:all).and_return nil
+      get :index
     end
 
     context 'when the current user is an admin' do
       let(:current_user) { FactoryBot.create :admin }
+
       it 'returns http success' do
-        get :index
         expect(response).to have_http_status(:success)
       end
 
       it 'renders index page' do
-        expect(get(:index)).to render_template('hosts/index')
+        expect(response).to render_template('hosts/index')
       end
     end
 
-    context 'current user is not an admin' do
+    context 'when the current user is not an admin' do
       let(:current_user) { FactoryBot.create :user }
-
-      before :each do
-        get(:index)
-      end
 
       it 'returns http redirect' do
         expect(response).to have_http_status(:redirect)
-      end
-
-      it 'notifies about missing rights' do
-        should set_flash.now[:alert].to('You do not have the necessary rights to perform this action.')
       end
     end
   end
@@ -49,45 +42,50 @@ RSpec.describe HostsController, type: :controller do
     before do
       allow(VmApi).to receive(:instance).and_return double_api
     end
+
     context 'when the current user is an admin' do
       let(:current_user) { FactoryBot.create :admin }
 
-      it 'returns http success or timeout or not found' do
-        allow(double_api).to receive(:get_host).and_return({})
-        get :show, params: { id: 1 }
-        expect(response).to have_http_status(:success).or have_http_status(408).or have_http_status(:not_found)
+      context 'when there is an host' do
+        before do
+          allow(double_api).to receive(:get_host).and_return({})
+          get :show, params: { id: 1 }
+        end
+
+        it 'returns http success or timeout or not found' do
+          expect(response).to have_http_status(:success).or have_http_status(408).or have_http_status(:not_found)
+        end
+
+        it 'renders show page' do
+          expect(response).to render_template('hosts/show')
+        end
       end
 
-      it 'renders show page' do
-        allow(double_api).to receive(:get_host).and_return({})
-        expect(get(:show, params: { id: 1 })).to render_template('hosts/show')
-      end
+      context 'when there is no host' do
+        before do
+          allow(double_api).to receive(:get_host).and_return(nil)
+          get :show, params: { id: 1 }
+        end
 
-      it 'returns http status not found when no host found' do
-        allow(double_api).to receive(:get_host).and_return(nil)
-        get :show, params: { id: 5 }
-        expect(response).to have_http_status(:not_found)
-      end
+        it 'returns http status not found when no host found' do
+          expect(response).to have_http_status(:not_found)
+        end
 
-      it 'renders not found page when no host found' do
-        allow(double_api).to receive(:get_host).and_return(nil)
-        expect(get(:show, params: { id: 1 })).to render_template('errors/not_found')
+        it 'renders not found page when no host found' do
+          expect(response).to render_template('errors/not_found')
+        end
       end
     end
 
-    context 'current user is not an admin' do
+    context 'when the current user is not an admin' do
       let(:current_user) { FactoryBot.create :user }
 
-      before :each do
+      before do
         get(:show, params: { id: 1 })
       end
 
       it 'returns http redirect' do
         expect(response).to have_http_status(:redirect)
-      end
-
-      it 'notifies about missing rights' do
-        should set_flash.now[:alert].to('You do not have the necessary rights to perform this action.')
       end
     end
   end
