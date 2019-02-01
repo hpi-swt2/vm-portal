4062db5665993ae9a3b4a74d7510494e4f40847f
Maximilian <koenigmax1@web.de>
2019-02-01 13:46:07 +0100

    Modify migration again

--- db/migrate/20190131115425_rename_request_mb_to_gb.rb
+++ db/migrate/20190131115425_rename_request_mb_to_gb.rb
@@ -2,16 +2,16 @@
 
 class RenameRequestMbToGb < ActiveRecord::Migration[5.2]
   def up
-    update_requests
     rename_column :requests, :ram_mb, :ram_gb
     rename_column :requests, :storage_mb, :storage_gb
+    update_requests
   end
 
   def update_requests
     Request.all.each do |request|
       unless request.nil?
-        request.ram_mb = (request.ram_mb / 1000).to_f.ceil
-        request.storage_mb = (request.storage_mb / 1000).to_f.ceil
+        request.ram_gb = (request.ram_gb / 1000.0).ceil
+        request.storage_gb = (request.storage_gb / 1000.0).ceil
       end
       request.save
     end
