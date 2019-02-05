1c7b71b460c5c6ca31781037d7da03be9f06872f
Adrian Steppat <adrian.steppat@student.hpi.de>
2019-02-05 22:15:17 +0100

    Remove redundant begin block

--- app/models/request.rb
+++ app/models/request.rb
@@ -85,15 +85,13 @@ class Request < ApplicationRecord
   end
 
   def push_to_git
-    begin
-      GitHelper.open_repository(PuppetParserHelper.puppet_script_path) do |git_writer|
-        git_writer.write_file(File.join('Node', "node_#{name}.pp"), generate_puppet_node_script)
-        git_writer.write_file(File.join('Name', "#{name}.pp"), generate_puppet_name_script)
-        git_writer.save(commit_message(git_writer))
-      end
-    rescue Git::GitExecuteError => e
-      logger.error(e)
+    GitHelper.open_repository(PuppetParserHelper.puppet_script_path) do |git_writer|
+      git_writer.write_file(File.join('Node', "node_#{name}.pp"), generate_puppet_node_script)
+      git_writer.write_file(File.join('Name', "#{name}.pp"), generate_puppet_name_script)
+      git_writer.save(commit_message(git_writer))
     end
+  rescue Git::GitExecuteError => e
+    logger.error(e)
   end
 
   def commit_message(git_writer)
