d608b84d854ad71b20c7a4ec4d82a8caf55246d3
regnujAx <alexander.junger@student.hpi.uni-potsdam.de>
2019-02-05 09:58:45 +0000

    archive notificationwill be sent in 3 days

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
+  queue_as :send_notification
+
+  def perform(admins_ids, vm_name, vm_url)
+	admins = User.where(id: admins_ids)
+   	admins.each do |each|
+   	  each.notify("VM #{vm_name} has been requested to be archived",
+				  "The VM has been shut down and has to be archived.\n#{vm_url}")
+      end
+  end
 end
