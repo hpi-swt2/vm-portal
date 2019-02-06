da12916adf819e7469101c65e2e6ed2a7b5b00b5
Melvin Witte <melvin.witte@student.hpi.de>
2019-02-06 14:35:19 +0100

    Fix dashboard and vms index page loading times for users with many vms

--- app/api/v_sphere/virtual_machine.rb
+++ app/api/v_sphere/virtual_machine.rb
@@ -292,6 +292,11 @@ module VSphere
       users
     end
 
+    def users_without_pull
+      remote_users = Puppetscript.read_node_file(name)
+      remote_users['users']
+    end
+
     def commit_message(git_writer)
       if git_writer.added?
         'Add ' + name
@@ -326,9 +331,9 @@ module VSphere
         git_writer.write_file(node_path, node_script)
         message = commit_message(git_writer)
         git_writer.save(message)
-      rescue Git::GitExecuteError => e
-        Rails.logger.error(e)
       end
+    rescue Git::GitExecuteError => e
+      Rails.logger.error(e)
     end
 
     def sudo_users
@@ -360,9 +365,9 @@ module VSphere
         git_writer.write_file(node_path, node_script)
         message = commit_message(git_writer)
         git_writer.save(message)
-      rescue Git::GitExecuteError => e
-        logger.error(e)
       end
+    rescue Git::GitExecuteError => e
+      logger.error(e)
     end
 
     # fine to use for a single vm. If you need to check multiple vms for a user, check with user_vms
--- lib/git_helper.rb
+++ lib/git_helper.rb
@@ -2,6 +2,10 @@
 
 module GitHelper
   def self.open_repository(path)
+    3.times do
+      puts 'Open Git'
+      puts '//////////////////////'
+    end
     FileUtils.mkdir_p(path) unless File.exist?(path)
     git_writer = GitWriter.new(path)
     yield git_writer
