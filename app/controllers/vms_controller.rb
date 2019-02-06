3eeb4a8574a69dc5e6c1a0d6a63f0e828bfa959b
Leon Matthes <leon@matthes.biz>
2019-02-06 15:59:37 +0100

    Fix a shitload of bugs

--- app/controllers/vms_controller.rb
+++ app/controllers/vms_controller.rb
@@ -55,11 +55,12 @@ class VmsController < ApplicationController
   end
 
   def update
-    notify_changed_users(@vm.sudo_users.map(&:id), info_params[:vm_info][:sudo_user_ids].map(&:to_i), true, @vm.name)
-    notify_changed_users(@vm.users.map(&:id), info_params[:vm_info][:non_sudo_user_ids].map(&:to_i), false, @vm.name)
-    @vm.sudo_users = info_params[:vm_info][:sudo_user_ids]
-    @vm.users = info_params[:vm_info][:non_sudo_user_ids]
-    @vm.config.description = info_params[:vm_info][:description]
+    prepare_info_params
+    notify_changed_users(@vm.sudo_users.map(&:id), info_params[:sudo_user_ids].map(&:to_i), true, @vm.name)
+    notify_changed_users(@vm.users.map(&:id), info_params[:non_sudo_user_ids].map(&:to_i), false, @vm.name)
+    @vm.sudo_users = info_params[:sudo_user_ids]
+    @vm.users = info_params[:non_sudo_user_ids]
+    @vm.ensure_config.description = info_params[:description]
     unless @vm.config.save
       flash[:error] = 'Description couldn\'t be saved.'
       redirect_to edit_vm_path(@vm.name)
@@ -163,6 +164,13 @@ class VmsController < ApplicationController
   # Because no Active Record model is being wrapped
 
   private
+
+  def prepare_info_params
+    return unless params[:vm_info]
+
+    params[:vm_info][:sudo_user_ids] ||= @vm.sudo_users.map(&:id)
+    params[:vm_info][:non_sudo_user_ids] ||= (@vm.users - @vm.sudo_users).map(&:id)
+  end
 
   def initialize_vm_categories
     @vms = VSphere::VirtualMachine.rest
