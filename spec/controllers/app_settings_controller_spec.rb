417fff9ed35de0d8b9783828a6817ea22167faf7
Björn Daase <bjoern.daase@student.hpi.de>
2019-02-19 11:25:06 +0100

    Remove unnecessary test
    Co-authored-by: Leon Matthes <leon@matthes.biz>

--- spec/controllers/app_settings_controller_spec.rb
+++ spec/controllers/app_settings_controller_spec.rb
@@ -41,56 +41,11 @@ RSpec.describe AppSettingsController, type: :controller do
   # AppSettingsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
-      AppSetting.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #show" do
-    it "returns a success response" do
-      app_setting = AppSetting.create! valid_attributes
-      get :show, params: {id: app_setting.to_param}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_successful
-    end
-  end
-
   describe "GET #edit" do
     it "returns a success response" do
       app_setting = AppSetting.create! valid_attributes
       get :edit, params: {id: app_setting.to_param}, session: valid_session
       expect(response).to be_successful
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new AppSetting" do
-        expect {
-          post :create, params: {app_setting: valid_attributes}, session: valid_session
-        }.to change(AppSetting, :count).by(1)
-      end
-
-      it "redirects to the created app_setting" do
-        post :create, params: {app_setting: valid_attributes}, session: valid_session
-        expect(response).to redirect_to(AppSetting.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {app_setting: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
-      end
     end
   end
 
@@ -120,21 +75,6 @@ RSpec.describe AppSettingsController, type: :controller do
         put :update, params: {id: app_setting.to_param, app_setting: invalid_attributes}, session: valid_session
         expect(response).to be_successful
       end
-    end
-  end
-
-  describe "DELETE #destroy" do
-    it "destroys the requested app_setting" do
-      app_setting = AppSetting.create! valid_attributes
-      expect {
-        delete :destroy, params: {id: app_setting.to_param}, session: valid_session
-      }.to change(AppSetting, :count).by(-1)
-    end
-
-    it "redirects to the app_settings list" do
-      app_setting = AppSetting.create! valid_attributes
-      delete :destroy, params: {id: app_setting.to_param}, session: valid_session
-      expect(response).to redirect_to(app_settings_url)
     end
   end
 
