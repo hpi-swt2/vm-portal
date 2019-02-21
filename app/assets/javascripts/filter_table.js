7d0e4b4866cc22653fa2cc82e62ce1c40db580cb
Tom Braun <brauntom2@t-online.de>
2019-02-21 13:44:00 +0100

    there is only one search bar on the vms page the search bar searchs in all categories applied refactorings according to #504

--- app/assets/javascripts/filter_table.js
+++ app/assets/javascripts/filter_table.js
@@ -1,21 +1,46 @@
-function filter_table(input_id, table_id, doc) {
-  // Declare variables 
-  var input, filter, table, tr, td, i, j, txtValue;
-  input = doc.getElementById(input_id);
-  filter = input.value.toUpperCase();
-  table = doc.getElementById(table_id);
-  tr = table.getElementsByTagName("tr");
-  
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td");
-    for (j = 0; j < td.length; j++) {
-        txtValue = td[j].textContent || td[j].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break;
+function hide(htmlElm) {
+    htmlElm.style.display = 'none';
+}
+
+function unhide(htmlElm) {
+    htmlElm.style.display = '';
+}
+
+function someCellInRowContains(row, searchedText) {
+    return Array.prototype.some.call(row, cell => cell.textContent.toUpperCase().indexOf(searchedText) !== -1)
+}
+
+function filterTable(inputId, tableId) {
+    const input = document.getElementById(inputId);
+    const table = document.getElementById(tableId);
+
+    const filter = input.value.toUpperCase();
+    const rows = table.getElementsByTagName('tr');
+    let invisible = 0;
+
+    // Loop through all table rows, and hide those who don't match the search query ignoring the headline
+    for (let i = 1; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('td');
+
+        if(someCellInRowContains(cells, filter)) {
+            unhide(rows[i]);
         }
-        tr[i].style.display = "none";
+        else {
+            hide(rows[i]);
+            invisible++;
+        }
     }
-  }
+
+    if(invisible === tr.length - 1) {
+        hide(table);
+    }
+    else {
+        unhide(table);
+    }
+}
+
+function filterTables(inputId, tableIds){
+    for(const tableID of tableIds){
+        filterTable(inputId, tableID)
+    }
 }
