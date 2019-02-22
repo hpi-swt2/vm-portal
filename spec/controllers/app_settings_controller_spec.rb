6667485fd11b20ce1af083a6e0ab498a79454059
Leon Matthes <leon@matthes.biz>
2019-02-22 13:00:12 +0100

    Update test

--- spec/controllers/app_settings_controller_spec.rb
+++ spec/controllers/app_settings_controller_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe AppSettingsController, type: :controller do
       it 'updates the mailer settings' do
         Rails.configuration.action_mailer.smtp_settings[:address] = 'Hello World!'
         put :update, params: { id: AppSetting.instance.to_param, app_setting: valid_attributes }
-        expect(Rails.configuration.action_mailer.smtp_settings[:address]).to eql(valid_attributes[:email_notification_smtp_address])
+        expect(ActionMailer::Base.smtp_settings[:address]).to eql(valid_attributes[:email_notification_smtp_address])
       end
 
       it 'resets the git settings' do
