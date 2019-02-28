9f714f66fe7c778f89895df19295fa539d5c48b6
Leon Matthes <leon@matthes.biz>
2019-02-28 19:50:54 +0100

    Use delete instead of post for destroy_and_redirect

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,7 @@ Rails.application.routes.draw do
   resources :notifications, only: %i[index new create destroy] do
     get :mark_as_read, on: :member
     get :has_any, on: :collection, to: 'notifications#any?'
-    member do
-      post 'destroy_and_redirect'
-    end
+    delete :destroy_and_redirect, on: :member
   end
 
   get '/dashboard' => 'dashboard#index', as: :dashboard
