6c9c4340bf07648a8957c9c47c45f40948ea607c
Björn Daase <bjoern.daase@student.hpi.de>
2019-03-01 22:36:37 +0100

    Remove RequestsHelper, that does automatic conversion of names

--- app/helpers/requests_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module RequestsHelper
-  # replaces all whitespaces and special characters by a hyphen and makes the string to lower case
-  def replace_whitespaces(name)
-    name.parameterize
-  end
-end
