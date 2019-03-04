6f6666798be3447ce2f851530945f2a733457db3
Björn Daase <bjoern.daase@student.hpi.de>
2019-03-04 21:58:45 +0100

    Fix error messages for requests

--- app/models/request.rb
+++ app/models/request.rb
@@ -33,8 +33,8 @@ class Request < ApplicationRecord
   end
 
   def name_uniqueness
-    errors.add('vSphere already has a VM with the same name') if VSphere::VirtualMachine.all.map(&:name).include?(name)
-    errors.add('There is already a request with the same name') if (Request.pending + Request.accepted).map(&:name).include?(name)
+    errors.add(:name, ': vSphere already has a VM with the same name') if VSphere::VirtualMachine.all.map(&:name).include?(name)
+    errors.add(:name, ': There is already a request with the same name') if (Request.pending + Request.accepted).map(&:name).include?(name)
   end
 
   def description_text(host_name)
