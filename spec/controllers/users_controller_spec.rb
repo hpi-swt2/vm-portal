769f9216d375fc4291ffa2c02aa4603ae9f84e6e
Tom Braun <brauntom2@t-online.de>
2019-03-06 17:12:11 +0100

    put check in helper-method

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:user) { FactoryBot.create :user }
-  let(:admin) { FactoryBot.create :admin }
+  let(:user) {FactoryBot.create :user}
+  let(:admin) {FactoryBot.create :admin}
 
   describe 'GET #index' do
     context 'when the user is an admin' do
@@ -19,28 +19,27 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-    context 'when the user is not an admin' do
-      before do
-        sign_in user
-      end
+  context 'when the user is not an admin' do
+    before do
+      sign_in user
+    end
 
-      it 'redirects to the dashboard' do
-        get :index
-        expect(response).to redirect_to(dashboard_path)
-      end
+    it 'redirects to the dashboard' do
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+    end
 
-      it 'returns http success for normal users in development environment' do
-        allow(Rails.env).to receive(:development?).and_return(true)
-        get :index
-        expect(response).to have_http_status(:success)
-      end
+    it 'returns http success for normal users in development environment' do
+      allow(Rails.env).to receive(:development?).and_return(true)
+      get :index
+      expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET #show' do
     it 'returns http success' do
       sign_in admin
-      get :show, params: { id: user }
+      get :show, params: {id: user}
       expect(response).to have_http_status(:success)
     end
   end
@@ -48,7 +47,7 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #edit' do
     it 'returns http success' do
       sign_in user
-      get :edit, params: { id: user }
+      get :edit, params: {id: user}
       expect(response).to have_http_status(:success)
     end
   end
