9327fbc722b1ffe82f11e04d10e6e2dc15c40f8b
Leon Matthes <leon@matthes.biz>
2019-03-16 18:05:07 +0100

    Don't use custom regex for server validation

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'resolv'
+
 class Server < ApplicationRecord
   belongs_to :responsible, class_name: :User
   validates :name, :cpu_cores, :ram_gb, :storage_gb, :mac_address, :fqdn, :responsible_id, presence: true
@@ -12,21 +14,15 @@ class Server < ApplicationRecord
   validates :storage_gb, numericality: { greater_than: 0 }
 
   validates :ipv4_address, format: {
-    with: /(^$)|((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/,
-    message: 'Not a valid IPv4 address'
+    with: Resolv::IPv4::Regex,
+    message: 'is not a valid IPv4 address'
   }
   validates :ipv6_address, format: {
-    with: /(^$)|(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:)
-      {1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}
-        (:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}
-        (:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)
-        |fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}
-        [0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|
-        (2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/x,
-    message: 'Not a valid IPv6 address'
+    with: Resolv::IPv6::Regex,
+    message: 'is not a valid IPv6 address'
   }
   validates :mac_address, format: {
     with: /([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})/,
-    message: 'Not a valid MAC address'
+    message: 'is not a valid MAC address'
   }
 end
