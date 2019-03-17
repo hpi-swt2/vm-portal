caef2e6f61e9cd8b85e86ebbe21435c268846d25
Björn Daase <bjoern.daase@student.hpi.de>
2019-03-17 19:42:40 +0100

    Remove unnecessary tests and reanable skipped test

--- spec/models/app_setting_spec.rb
+++ spec/models/app_setting_spec.rb
@@ -2,6 +2,8 @@
 
 require 'rails_helper'
 
+# We should add some tests here, which test that the validation
+
 RSpec.describe AppSetting, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
