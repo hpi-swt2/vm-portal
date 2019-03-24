255d71f8ab8a8d41495fbc60db2f3a5af3661bd7
Leon Matthes <leon@matthes.biz>
2019-03-24 19:19:06 +0100

    Fix more tests

--- spec/views/vms/edit.html.erb_spec.rb
+++ spec/views/vms/edit.html.erb_spec.rb
@@ -3,28 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'vms/edit.html.erb', type: :view do
-  let(:vm) do
-    v_sphere_vm_mock 'VM', vm_ware_tools: 'toolsInstalled'
-  end
   let(:configuration) do
-    config = double
-    allow(config).to receive(:description).and_return('hello world')
-    allow(config).to receive(:ip).and_return('127.0.0.1')
-    allow(config).to receive(:dns).and_return('8.8.8.8')
-    config
+    FactoryBot.create(:virtual_machine_config,
+                      name: 'VM',
+                      description: 'hello world',
+                      ip: '127.0.0.1',
+                      dns: '8.8.8.8')
   end
 
   let(:current_user) { FactoryBot.create :user }
 
   before do
     sign_in current_user
-    assign(:vm, vm)
     assign(:configuration, configuration)
     render
   end
 
   it 'shows vm name' do
-    expect(rendered).to have_text vm.name
+    expect(rendered).to have_text configuration.name
   end
 
   it 'says edit' do
