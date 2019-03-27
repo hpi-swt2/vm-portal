9dd632aa3d9ad81513319e1dec6ad48322585392
Christoph Matthies <chrisma@users.noreply.github.com>
2019-03-27 23:22:35 +0100

    Fix(Operating Systems Controller): Use I18n flash messages

--- app/controllers/operating_systems_controller.rb
+++ app/controllers/operating_systems_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class OperatingSystemsController < ApplicationController
+  @@resource_name = OperatingSystem.model_name.human.titlecase
+
   before_action :set_operating_system, only: %i[edit update destroy]
   before_action :authenticate_admin
 
@@ -21,7 +23,8 @@ class OperatingSystemsController < ApplicationController
   def create
     @operating_system = OperatingSystem.new(operating_system_params)
     if @operating_system.save
-      redirect_to operating_systems_url, notice: 'Operating system was successfully created.'
+      redirect_to operating_systems_path, notice: t('flash.actions.create.notice_detail',
+        resource: @@resource_name, model: @operating_system.name)
     else
       render :new
     end
@@ -30,7 +33,8 @@ class OperatingSystemsController < ApplicationController
   # PATCH/PUT /operating_systems/1
   def update
     if @operating_system.update(operating_system_params)
-      redirect_to operating_systems_url, notice: 'Operating system was successfully updated.'
+      redirect_to operating_systems_path, notice: t('flash.actions.update.notice_detail',
+        resource: @@resource_name, model: @operating_system.name)
     else
       render :edit
     end
@@ -38,8 +42,12 @@ class OperatingSystemsController < ApplicationController
 
   # DELETE /operating_systems/1
   def destroy
-    @operating_system.destroy
-    redirect_to operating_systems_url, notice: 'Operating system was successfully deleted.'
+    if @operating_system.destroy
+      notice = t('flash.actions.destroy.notice_detail', resource: @@resource_name, model: @operating_system.name)
+    else
+      alert = t('flash.actions.destroy.alert_detail', resource: @@resource_name, model: @operating_system.name)
+    end
+    redirect_to operating_systems_path, notice: notice, alert: alert
   end
 
   private
