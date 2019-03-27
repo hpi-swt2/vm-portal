52989dff1b1a153d251f03cc653764bd88a33575
Leon Matthes <leon@matthes.biz>
2019-03-27 22:28:57 +0100

    Refactor tests

--- spec/helpers/hart_formatter_spec.rb
+++ spec/helpers/hart_formatter_spec.rb
@@ -4,17 +4,18 @@ require 'rails_helper'
 
 describe HartFormatter do
 
-  it 'notifies admins when an error is logged' do
+  before do
     # ensure we have at least one admin
     FactoryBot.create :admin
+  end
+
+  it 'notifies admins when an error is logged' do
     expect do
       Rails.logger.error('My Error')
     end.to change(Notification, :count).by(User.admin.size)
   end
 
   it 'does not notify admins when the log is not an error' do
-    # ensure at least one admin
-    FactoryBot.create :admin
     expect do
       Rails.logger.info('My Info')
     end.not_to change(Notification, :count)
